1efd8743d225658c31d6ea91bd76569cc57b71eb
RakheshKarnati <50054199+RakheshKarnati@users.noreply.github.com>
2020-08-01 11:52:09 +0530

    Add files via upload

---
+++ php/action_page.php
@@ -0,0 +1,35 @@
+<?php
+    if(isset($_POST['submit']))
+    {
+        $name = $_POST['name']; // Get Name value from HTML Form
+        $email_id = $_POST['email']; // Get Email Value
+        $subject = $_POST['subject']; // Get Mobile No
+        $message = $_POST['message']; // Get Message Value
+         
+        $to = "rakeshroshankarnati98@gmail.com"; // You can change here your Email
+        $subject = "'$name' has been sent a mail"; // This is your subject
+
+        // Always set content-type when sending HTML email
+        $headers = "MIME-Version: 1.0" . "\r\n";
+        $headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
+ 
+        // More headers
+        $headers .= 'From: Admin <admin@websapex.com>' . "\r\n"; // Give an email id on which you want get a reply. User will get a mail from this email id
+        $headers .= 'Cc: info@websapex.com' . "\r\n"; // If you want add cc
+        $headers .= 'Bcc: boss@websapex.com' . "\r\n"; // If you want add Bcc
+         
+        if(mail($to,$subject,$message,$headers)){
+            // Message if mail has been sent
+            echo "<script>
+                    alert('Mail has been sent Successfully.');
+                </script>";
+        }
+ 
+        else{
+            // Message if mail has been not sent
+            echo "<script>
+                    alert('EMAIL FAILED');
+                </script>";
+        }
+    }
+?>
